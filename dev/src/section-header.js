753110df608ab2e2f6c4c533133d62d6bee55a07
yuliia-ant <yuliia@refactorplus.com>
2023-12-25 10:00:40 +0200

    Feat: added mobile secondary menu

--- dev/src/section-header.js
+++ dev/src/section-header.js
@@ -1,69 +1,219 @@
+// class SidebarToggle extends HTMLElement {
+//   constructor() {
+//     super();
+
+//     const template = document.getElementById("sidebar-toggle-template");
+
+//     const burgerIcon = this.querySelector(".nav__icon--burger");
+//     const closeIcon = this.querySelector(".nav__icon--close");
+//     const shopButton = this.querySelector(".nav__link--Shop");
+//     const closeIconDesktop = this.querySelector(".nav__icon--close--desktop");
+//     const desktopWrapper = this.querySelector(".sidebar-nav-overlay--desktop");
+//     const links = this.querySelectorAll(".js-sidebar-nav__link");
+//     const backButtons = this.querySelectorAll(
+//       ".js-secondary-menu__top-bar--back"
+//     );
+//     const closeButtons = document.querySelectorAll(
+//       ".js-secondary-menu__top-bar--close"
+//     );
+
+//     const toggleIconsClasses = () => {
+//       this.querySelector(".sidebar-nav-wrapper--mobile").classList.toggle(
+//         "nav-active"
+//       );
+//       const icons = this.querySelectorAll(".nav__icon");
+//       icons.forEach((icon) => icon.classList.toggle("nav__icon--js-hidden"));
+//     };
+
+//     const openNav = () => {
+//       this.querySelector("#sidebar-menu-desktop").style.left = "0";
+//       this.querySelector("#sidebar-nav-overlay--desktop").style.display =
+//         "block";
+//     };
+
+//     const closeNav = () => {
+//       this.querySelector("#sidebar-menu-desktop").style.left = "-100%";
+//       this.querySelector("#sidebar-nav-overlay--desktop").style.display =
+//         "none";
+//     };
+
+//     burgerIcon.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       toggleIconsClasses();
+//       this.querySelector(".nav__icon--close").classList.toggle("close-active");
+//     });
+
+//     closeIcon.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       toggleIconsClasses();
+//     });
+
+//     shopButton.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       openNav();
+//       this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle(
+//         "nav-active"
+//       );
+//     });
+
+//     closeIconDesktop.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       closeNav();
+//       this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle(
+//         "nav-active"
+//       );
+//     });
+
+//     desktopWrapper.addEventListener("click", () => {
+//       closeNav();
+//       this.querySelector(".sidebar-nav-wrapper--desktop").classList.remove(
+//         "nav-active"
+//       );
+//     });
+
+//     links.forEach((link) =>
+//       link.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         link.nextElementSibling.classList.toggle("secondary-menu-open");
+//       })
+//     );
+
+//     backButtons.forEach((backButton) =>
+//       backButton.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         event.stopPropagation();
+//         document
+//           .querySelector(".secondary-menu-open")
+//           .classList.toggle("secondary-menu-open");
+//       })
+//     );
+
+//     closeButtons.forEach((closeButton) =>
+//       closeButton.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         toggleIconsClasses();
+//       })
+//     );
+//   }
+// }
+
+// customElements.define("sidebar-toggle", SidebarToggle);
+
 class SidebarToggle extends HTMLElement {
-  constructor() {
-    super();
+  connectedCallback() {
 
-    const template = document.getElementById("sidebar-toggle-template");
+    this.burgerIcon = this.querySelector(".nav__icon--burger");
+    this.closeIcon = this.querySelector(".nav__icon--close");
+    this.shopButton = this.querySelector(".nav__link--Shop");
+    this.closeIconDesktop = this.querySelector(".nav__icon--close--desktop");
+    this.desktopWrapper = this.querySelector(".sidebar-nav-overlay--desktop");
+    this.links = this.querySelectorAll(".js-sidebar-nav__link");
+    this.backButtons = this.querySelectorAll(".js-secondary-menu__top-bar--back");
+    this.closeButtons = document.querySelectorAll(".js-secondary-menu__top-bar--close");
 
-    const burgerIcon = this.querySelector(".nav__icon--burger");
-    const closeIcon = this.querySelector(".nav__icon--close");
-    const shopButton = this.querySelector(".nav__link--Shop");
-    const closeIconDesktop = this.querySelector(".nav__icon--close--desktop");
-    const desktopWrapper = this.querySelector(".sidebar-nav-overlay--desktop");
+    this.addEventListeners();
+  }
 
-    const toggleIconsClasses = () => {
-      this.querySelector(".sidebar-nav-wrapper--mobile").classList.toggle(
-        "nav-active"
-      );
+  disconnectedCallback() {
+    this.removeEventListeners();
+  }
+
+  addEventListeners() {
+    this.toggleIconsClasses = () => {
+      this.querySelector(".sidebar-nav-wrapper--mobile").classList.toggle("nav-active");
       const icons = this.querySelectorAll(".nav__icon");
       icons.forEach((icon) => icon.classList.toggle("nav__icon--js-hidden"));
     };
 
-    const openNav = () => {
+    this.openNav = () => {
       this.querySelector("#sidebar-menu-desktop").style.left = "0";
-      this.querySelector("#sidebar-nav-overlay--desktop").style.display =
-        "block";
+      this.querySelector("#sidebar-nav-overlay--desktop").style.display = "block";
     };
 
-    const closeNav = () => {
+    this.closeNav = () => {
       this.querySelector("#sidebar-menu-desktop").style.left = "-100%";
-      this.querySelector("#sidebar-nav-overlay--desktop").style.display =
-        "none";
+      this.querySelector("#sidebar-nav-overlay--desktop").style.display = "none";
     };
 
-    burgerIcon.addEventListener("click", (event) => {
+    this.burgerIcon.addEventListener("click", (event) => {
       event.preventDefault();
-      toggleIconsClasses();
+      this.toggleIconsClasses();
       this.querySelector(".nav__icon--close").classList.toggle("close-active");
     });
 
-    closeIcon.addEventListener("click", (event) => {
+    this.closeIcon.addEventListener("click", (event) => {
       event.preventDefault();
-      toggleIconsClasses();
+      this.toggleIconsClasses();
     });
 
-    shopButton.addEventListener("click", (event) => {
+    this.shopButton.addEventListener("click", (event) => {
       event.preventDefault();
-      openNav();
-      this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle(
-        "nav-active"
-      );
+      this.openNav();
+      this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle("nav-active");
     });
 
-    closeIconDesktop.addEventListener("click", (event) => {
+    this.closeIconDesktop.addEventListener("click", (event) => {
       event.preventDefault();
-      closeNav();
-      this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle(
-        "nav-active"
-      );
+      this.closeNav();
+      this.querySelector(".sidebar-nav-wrapper--desktop").classList.toggle("nav-active");
     });
 
-    desktopWrapper.addEventListener("click", () => {
-      closeNav();
-      this.querySelector(".sidebar-nav-wrapper--desktop").classList.remove(
-        "nav-active"
-      );
+    this.desktopWrapper.addEventListener("click", () => {
+      this.closeNav();
+      this.querySelector(".sidebar-nav-wrapper--desktop").classList.remove("nav-active");
     });
 
+    this.links.forEach((link) =>
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        link.nextElementSibling.classList.toggle("secondary-menu-open");
+      })
+    );
+
+    this.backButtons.forEach((backButton) =>
+      backButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        document.querySelector(".secondary-menu-open").classList.toggle("secondary-menu-open");
+      })
+    );
+
+    this.closeButtons.forEach((closeButton) =>
+      closeButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        this.toggleIconsClasses();
+      })
+    );
+  }
+
+  removeEventListeners() {
+    this.burgerIcon.removeEventListener("click", this.toggleIconsClasses);
+    this.closeIcon.removeEventListener("click", this.toggleIconsClasses);
+    this.shopButton.removeEventListener("click", this.openNav);
+    this.closeIconDesktop.removeEventListener("click", this.closeNav);
+    this.desktopWrapper.removeEventListener("click", this.closeNav);
+
+    this.links.forEach((link) =>
+      link.removeEventListener("click", (event) => {
+        event.preventDefault();
+        link.nextElementSibling.classList.toggle("secondary-menu-open");
+      })
+    );
+
+    this.backButtons.forEach((backButton) =>
+      backButton.removeEventListener("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        document.querySelector(".secondary-menu-open").classList.toggle("secondary-menu-open");
+      })
+    );
+
+    this.closeButtons.forEach((closeButton) =>
+      closeButton.removeEventListener("click", (event) => {
+        event.preventDefault();
+        this.toggleIconsClasses();
+      })
+    );
   }
 }
 
